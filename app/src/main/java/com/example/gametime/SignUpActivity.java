214f598cd96d627b3351a2b332f0003a8a0a6e32
CodeMakesMeHowell <annachester02@gmail.com>
2022-07-26 22:13:13 -0400

    Implemented user creation and login features

--- app/src/main/java/com/example/gametime/SignUpActivity.java
+++ app/src/main/java/com/example/gametime/SignUpActivity.java
@@ -1,17 +1,32 @@
 package com.example.gametime;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.Spinner;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class SignUpActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+import com.example.gametime.model.Event;
+import com.example.gametime.model.User;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+
+public class SignUpActivity extends AppCompatActivity {
+    private EditText editTextRegisterFullName, editTextRegisterUsername, editTextRegisterPassword;
+    DatabaseReference ref = FirebaseDatabase.getInstance().getReferenceFromUrl("https://gametime-4360d-default-rtdb.firebaseio.com/");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,19 +38,62 @@ public class SignUpActivity extends AppCompatActivity implements AdapterView.OnI
         signInTxt.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(SignUpActivity.this, MainActivity.class);
-                startActivity(intent);
+                Intent intentSignIn = new Intent(SignUpActivity.this, MainActivity.class);
+                startActivity(intentSignIn);
                 finish();
             }
         });
-    }
 
-    @Override
-    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-        String text = parent.getItemAtPosition(position).toString();
-    }
+        editTextRegisterFullName = findViewById(R.id.name);
+        editTextRegisterUsername = findViewById(R.id.username);
+        editTextRegisterPassword = findViewById(R.id.password);
 
-    @Override
-    public void onNothingSelected(AdapterView<?> parent){
+        final Button buttonRegister = findViewById(R.id.registerBtn);
+
+        buttonRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String fullNameTxt = editTextRegisterFullName.getText().toString();
+                String usernameTxt = editTextRegisterUsername.getText().toString();
+                String passwordTxt = editTextRegisterPassword.getText().toString();
+
+                if (TextUtils.isEmpty(fullNameTxt)) {
+                    Toast.makeText(SignUpActivity.this, "Please enter your full name", Toast.LENGTH_SHORT).show();
+                    editTextRegisterFullName.setError("Full name required");
+                    editTextRegisterFullName.requestFocus();
+                } else if (TextUtils.isEmpty(usernameTxt)) {
+                    Toast.makeText(SignUpActivity.this, "Please enter a username", Toast.LENGTH_SHORT).show();
+                    editTextRegisterUsername.setError("Username is required");
+                    editTextRegisterUsername.requestFocus();
+                } else if (TextUtils.isEmpty(passwordTxt)) {
+                    Toast.makeText(SignUpActivity.this, "Please enter a password", Toast.LENGTH_SHORT).show();
+                    editTextRegisterPassword.setError("Password is required");
+                    editTextRegisterPassword.requestFocus();
+                } else  {
+                        ref.child("users").child("customers").addListenerForSingleValueEvent(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                            if(dataSnapshot.hasChild(usernameTxt))
+                            {
+                                Toast.makeText(SignUpActivity.this, "Username is already taken. Please enter another username.", Toast.LENGTH_LONG).show();
+                            } else  {
+                                User newUser = new User(fullNameTxt, usernameTxt, passwordTxt, new ArrayList<Event>());
+                                newUser.addToDb();
+
+                                Toast.makeText(SignUpActivity.this, "Registered successfully!", Toast.LENGTH_LONG).show();
+                                Intent intentSignIn = new Intent(SignUpActivity.this, MainActivity.class);
+                                startActivity(intentSignIn);
+                                finish();
+                            }
+                        }
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                        }
+                    });
+                }
+            }
+        });
     }
 }
--- app/src/main/java/com/example/gametime/model/User.java
+++ app/src/main/java/com/example/gametime/model/User.java
@@ -1,5 +1,56 @@
 package com.example.gametime.model;
 
+import android.content.Intent;
+import android.widget.Toast;
+
+import com.example.gametime.MainActivity;
+import com.example.gametime.SignUpActivity;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
+
 public class User {
-    //TODO
+    Boolean admin;
+    String name;
+    String username;
+    String password;
+    ArrayList<Event> events;
+    DatabaseReference ref = FirebaseDatabase.getInstance().getReferenceFromUrl("https://gametime-4360d-default-rtdb.firebaseio.com/");
+
+    public User(){
+    }
+
+    public User(String name, String username, String password){
+        this.name = name;
+        this.username = username;
+        this.password = password;
+        admin = true;
+    }
+
+    public User(String name, String username, String password, ArrayList<Event> events){
+        this.name = name;
+        this.username = username;
+        this.password = password;
+        this.events =  events;
+        events.add(new Event());
+        admin = false;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void addToDb(){
+        System.out.println(admin);
+        if(this.admin)
+        {
+            ref.child("users").child("admin").child(username).child("name").setValue(this.name);
+            ref.child("users").child("admin").child(username).child("password").setValue(this.password);
+        } else {
+            ref.child("users").child("customer").child(username).child("name").setValue(this.name);
+            ref.child("users").child("customer").child(username).child("password").setValue(this.password);
+            ref.child("users").child("customer").child(username).child("events").setValue(this.events);
+        }
+    }
 }
